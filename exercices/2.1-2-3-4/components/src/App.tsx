d2c3ed0f966988faccede1459ccb21e70ffe63e4
mitica-criste-vinci <mitica.criste@student.vinci.be>
2025-10-08 15:03:00 +0200

    new ex2.2

--- exercices/2.1-2-3-4/components/src/App.tsx
+++ exercices/2.1-2-3-4/components/src/App.tsx
@@ -1,3 +1,6 @@
+/*
+ex2.1 
+
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
 
@@ -62,22 +65,103 @@ const PageTitle = (props: HeaderProps) => {
 
 
 const Cinema = (props: CinemaProps) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <ul>
-        <li>
-          <strong>{props.movie1Title}</strong> - Réalisateur : {props.movie1Director}
-        </li>
-        <li>
-          <strong>{props.movie2Title}</strong> - Réalisateur : {props.movie2Director}
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <ul>
+          <li>
+            <strong>{props.movie1Title}</strong> - Réalisateur : {props.movie1Director}
+          </li>
+          <li>
+            <strong>{props.movie2Title}</strong> - Réalisateur : {props.movie2Director}
+          </li>
+        </ul>
+      </div>
+    );
 };
 
 
 
 
+export default App;
+
+*/
+
+const App = () => {
+  const pageTitle = "Informations sur les films dans les cinémas";
+
+  const cinema1Name = "UGC DeBrouckère";
+
+  const movie1 = {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  };
+  const movie2 = {
+    title: "GOODBYE JULIA ",
+    director: "Mohamed Kordofani",
+  };
+
+  const cinema2Name = "UGC Toison d'Or";
+  const movie3 = {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  };
+  const movie4 = {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  };
+
+  return (
+    <div>
+      <PageTitle title={pageTitle} />
+
+      <Cinema name={cinema1Name} movie1={movie1} movie2={movie2} />
+
+      <Cinema name={cinema2Name} movie1={movie3} movie2={movie4} />
+    </div>
+  );
+};
+
+
+type Movie = {
+  title : string,
+  director : string
+}
+
+
+type PageTitleProps = {
+  title: string;
+};
+
+type CinemaProps = {
+  name: string;
+  movie1: Movie;
+  movie2: Movie;
+};
+
+
+const PageTitle = (props : PageTitleProps) => {
+  return(
+     <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+    </header>
+  );
+}
+
+
+const Cinema = (props : CinemaProps) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <ul>
+        <li>
+          <strong>{props.movie1.title}</strong> - Réalisateur : {props.movie1.director}
+        </li>
+        <li>
+          <strong>{props.movie2.title}</strong> - Réalisateur : {props.movie2.director}
+        </li>
+      </ul>
+    </div>
+  );
+}
 export default App;
