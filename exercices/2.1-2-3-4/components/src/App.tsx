f032f07416b6a71d35f42252cdc082e35c9184f4
mitica-criste-vinci <mitica.criste@student.vinci.be>
2025-10-13 15:16:16 +0200

    starting 2.3

--- exercices/2.1-2-3-4/components/src/App.tsx
+++ exercices/2.1-2-3-4/components/src/App.tsx
@@ -122,12 +122,10 @@ const App = () => {
   );
 };
 
-
 type Movie = {
-  title : string,
-  director : string
-}
-
+  title: string;
+  director: string;
+};
 
 type PageTitleProps = {
   title: string;
@@ -139,29 +137,29 @@ type CinemaProps = {
   movie2: Movie;
 };
 
-
-const PageTitle = (props : PageTitleProps) => {
-  return(
-     <header>
+const PageTitle = (props: PageTitleProps) => {
+  return (
+    <header>
       <h1 className="animate__animated animate__bounce">{props.title}</h1>
     </header>
   );
-}
+};
 
-
-const Cinema = (props : CinemaProps) => {
+const Cinema = (props: CinemaProps) => {
   return (
     <div>
       <h2>{props.name}</h2>
       <ul>
         <li>
-          <strong>{props.movie1.title}</strong> - Réalisateur : {props.movie1.director}
+          <strong>{props.movie1.title}</strong> - Réalisateur :{" "}
+          {props.movie1.director}
         </li>
         <li>
-          <strong>{props.movie2.title}</strong> - Réalisateur : {props.movie2.director}
+          <strong>{props.movie2.title}</strong> - Réalisateur :{" "}
+          {props.movie2.director}
         </li>
       </ul>
     </div>
   );
-}
+};
 export default App;
